8faba8aea0eca6176c46d0d90e851546bc6e8dbf
rasheedmhd <starletgh@gmail.com>
2024-05-08 17:15:27 +0000

    scrap, yay! crap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use reqwest;
+
 fn main() {
     println!("A web scrapper to detect career pages");
+    let site = "https://remoteintech.company/";
+    let response = reqwest::blocking::get(site);
+    let data = response.unwrap().text().unwrap();
+
+    println!("{data}");
 }
